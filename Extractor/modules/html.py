a4ed9ddbf3b98a2139bd145bc7e7defac552bb2b
Kratik <159747619+Kratik00@users.noreply.github.com>
2025-08-07 20:37:32 +0530

    Update html.py

--- Extractor/modules/html.py
+++ Extractor/modules/html.py
@@ -176,8 +176,7 @@ async def handle_txt_file(client, message: Message):
         txt_path = f"{file_base}.txt"
         html_path = f"{file_base}.html"
 
-        async for _ in client.download_media(message.document, file_name=txt_path):
-            pass
+        downloaded_path = await client.download_media(message=document, file_name=txt_path)
         with open(txt_path, 'r', encoding='utf-8', errors='ignore') as f:
             content = f.read()
         #os.remove(txt_path)
